86f505469934ed7f2dd962914df34a582312896c
li.shuo <444947389@qq.com>
2018-12-04 15:28:45 +0800

    header组件&axios封装

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { getSeller } from 'api'
+import VHeader from 'components/v-header/v-header'
 
 export default {
+  data() {
+    return {
+      seller: {}
+    }
+  },
   name: 'app',
+  created() {
+    getSeller().then((seller) => {
+      this.seller = seller
+    })
+  },
   components: {
-    HelloWorld
+    VHeader
   }
 }
 </script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
 </style>
