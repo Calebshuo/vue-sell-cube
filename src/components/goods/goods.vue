803c9b3fa59d248df98e4ffe18e6803fdaa846b7
li.shuo <444947389@qq.com>
2018-12-20 17:46:05 +0800

    nav-bar

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -7,6 +7,27 @@
         :options="scrollOptions"
         v-if="goods.length"
       >
+        <template slot="bar" slot-scope="props">
+          <cube-scroll-nav-bar 
+           direction="vertical"
+           :labels="props.labels"
+           :txts="barTxts"
+           :current="props.current">
+            <template slot-scope="props">
+              <div class="text">
+                <support-ico
+                  v-if="props.txt.type>=1"
+                  :size=3
+                  :type="props.txt.type"
+                ></support-ico>
+                <span>{{props.txt.name}}</span>
+                <span class="num" v-if="props.txt.count">
+                  <bubble :num="props.txt.count"></bubble>
+                </span>
+              </div>
+            </template>
+           </cube-scroll-nav-bar>
+        </template>
         <cube-scroll-nav-panel
           v-for="good in goods"
           :key="good.name"
@@ -58,8 +79,8 @@
   import CartControl from 'components/cart-control/cart-control'
   import ShopCart from 'components/shop-cart/shop-cart'
   // import Food from 'components/food/food'
-  // import SupportIco from 'components/support-ico/support-ico'
-  // import Bubble from 'components/bubble/bubble'
+  import SupportIco from 'components/support-ico/support-ico'
+  import Bubble from 'components/bubble/bubble'
 
   export default {
     name: 'goods',
@@ -96,22 +117,22 @@
         })
         return foods
       },
-      // barTxts() {
-      //   let ret = []
-      //   this.goods.forEach((good) => {
-      //     const {type, name, foods} = good
-      //     let count = 0
-      //     foods.forEach((food) => {
-      //       count += food.count || 0
-      //     })
-      //     ret.push({
-      //       type,
-      //       name,
-      //       count
-      //     })
-      //   })
-      //   return ret
-      // }
+      barTxts() {
+        let ret = []
+        this.goods.forEach((good) => {
+          const {type, name, foods} = good
+          let count = 0
+          foods.forEach((food) => {
+            count += food.count || 0
+          })
+          ret.push({
+            type,
+            name,
+            count
+          })
+        })
+        return ret
+      }
     },
     methods: {
       fetch() {
@@ -164,8 +185,8 @@
       }
     },
     components: {
-      // Bubble,
-      // SupportIco,
+      Bubble,
+      SupportIco,
       CartControl,
       ShopCart,
       // Food
@@ -201,14 +222,14 @@
       .text
         flex: 1
         position: relative
-      .num
-        position: absolute
-        right: -8px
-        top: -10px
-      .support-ico
-        display: inline-block
-        vertical-align: top
-        margin-right: 4px
+        .num
+          position: absolute
+          right: -8px
+          top: -10px
+        .support-ico
+          display: inline-block
+          vertical-align: top
+          margin-right: 4px
     >>> .cube-scroll-nav-bar-item_active
       background: $color-white
       color: $color-dark-grey
